86890f6f602559815abf80253158160d8a9885dc
luk-pio <l.piotrak@samsung.com>
2020-05-05 17:44:11 +0200

    Add dropbox

--- .spacemacs
+++ .spacemacs
@@ -160,8 +160,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Hack Nerd Font Mono"
-                               :size 20
+   dotspacemacs-default-font '("Hack"
+                               :size 12
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
-#git clone https://github.com/luk-pio/dots.git
-#cd dots
+email="l.piotrak@samsung.com"
+
+sudo apt install -y git curl
+git config git config --global user.name "luk-pio"
+git config --global user.email $email
+ssh-keygen -t rsa -b 4096 -C $email
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+# Dropbox
+curl -O "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+~/.dropbox-dist/dropboxd
+sudo apt install python
+sudo wget -O /usr/local/bin/dropbox "https://www.dropbox.com/download?dl=packages/dropbox.py"
+sudo chmod +x /usr/local/bin/dropbox
+sudo systemctl daemon-reload
+sudo systemctl enable dropbox
+sudo systemctl start dropbox
+
+git clone https://github.com/luk-pio/dotfs.git
+cd dotfs
 
 # Font
-sudo apt install fonts-hack-ttf
+sudo apt install -y fonts-hack-ttf
 
 # Spacemacs
-#sudo apt install emacs
-#git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-#git --git-dir=$HOME/.emacs.d/.git checkout develop
-#ln .spacemacs $HOME/.spacemacs
-#mkdir -p $HOME/emacs.d/private/org-roam/
-#ln .emacs.d/private/org-roam/packages.el $HOME/.emacs.d/private/org-roam/packages.el
-#cp emacs.service $HOME/.config/systemd/user/emacs.service
-#systemctl enable --user emacs
-#systemctl start --user emacs
+sudo apt install -y emacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git --git-dir=$HOME/.emacs.d/.git checkout develop
+ln .spacemacs $HOME/.spacemacs
+mkdir -p $HOME/emacs.d/private/org-roam/
+ln .emacs.d/private/org-roam/packages.el $HOME/.emacs.d/private/org-roam/packages.el
+cp emacs.service $HOME/.config/systemd/user/emacs.service
+systemctl enable --user emacs
+systemctl start --user emacs
 #
 ## vim
-#sudo apt install vim
+#sudo apt install -y vim
 ln .vimrc $HOME/.vimrc
 ln .vim/plugins.vim $HOME/.vim/plugins.vim
 #
 ## zsh
-sudo apt install zsh
+sudo apt install -y zsh
 mv $HOME/.zshrc $HOME/.zshrc.old
 ln .zshrc $HOME/.zshrc
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -36,7 +55,7 @@ ln .ideavimrc $HOME/.ideavimrc
 echo "Remeber to unzip pycharm configs"
 
 # Ranger
-#sudo apt install ranger
+#sudo apt install -y ranger
 #mv $HOME/.config/ranger $HOME/.config/ranger.old
 #cp -r .config/ranger $HOME/.config/ranger
 
