f5166359d1d0a186331e58ff4fe1ebe68d5d3afa
LukaszPiotrakArdigen <lukasz.piotrak@ardigen.com>
2022-05-19 11:22:08 +0200

    feat: clean up dotfiles and install script

--- run-syncthing.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-
-docker run -e STGUIADDRESS= --network=host -v $HOME/Dropbox/onyx:/var/syncthing syncthing/syncthing:latest
--- scripts/disable_fn_solaar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-solaar config 1 fn-swap f
-sleep 3
-solaar config 1 fn-swap f
