89a41238ba8188cc8d170d2be416df5e6c5b7f0a
luk-pio <luke.piotrak@github.com>
2025-01-10 20:53:09 +0000

    feat: simplify install script

--- install.sh
+++ install.sh
@@ -1,94 +1,17 @@
 #!/usr/bin/env bash
 
-# ---------------------------
-# -      Definitions        -
-# ---------------------------
-
 CWD="$(pwd)"
 DOTFS="$(pwd)"
 INSTALL_CMD="brew install"
 
-# $1: File path from ~
-symlink_confirm_overwrite() {
-  echo
-  PROMPT="Link $1? [y/n] "
-  read -p "$PROMPT" ANSWER
-  if [ -z "$ANSWER" ] || [ "$ANSWER" == "y" ]; then
-    if [ -f "$HOME/$1" ]; then
-      PROMPT="~/$1 already exists. Overwrite? [y/n]"
-      read -p "$PROMPT" ANSWER
-      if [ -z "$ANSWER" ] || [ "$ANSWER" != "y" ]; then
-        return 0
-      fi
-    fi
-    ln -s -f "$DOTFS/files/$1" "$HOME/$1"
-  fi 
-}
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+else 
+  echo "~/.oh-my-zsh already exists. Skipping installation."
+fi
 
-# $1: Prompt message, $2: Command to execute
-do_if_confirmation() {
-  echo
-  PROMPT="$1 [y/n] "
-  read -p "$PROMPT" ANSWER
-  if [ -z "$ANSWER" ] || [ "$ANSWER" == "y" ]; then
-    eval $2
-  fi 
-}
+# curl "https://raw.githubusercontent.com/pyrho/hack-font-ligature-nerd-font/master/font/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf" Hack_Regular_Nerd_Font_Mono.ttf
 
-# ---------------------------
-# -         Shell           -
-# ---------------------------
-
-# zsh
-symlink_confirm_overwrite .zshrc
-do_if_confirmation "Do you want to set up oh-my-zsh?" '
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-'
-symlink_confirm_overwrite .p10k.zsh
-
-# ---------------------------
-# -      Shell Utils        -
-# ---------------------------
-
-# ssh
-mkdir $HOME/.ssh
-symlink_confirm_overwrite .ssh/config
-
-# git
-symlink_confirm_overwrite .gitconfig
-
-# tmux
-symlink_confirm_overwrite .tmux.conf
-
-# aliases
-symlink_confirm_overwrite .aliasrc
-
-# ranger
-do_if_confirmation "Do you want to set up ranger?" '$INSTALL ranger'
-
-# ---------------------------
-# -         Editors         -
-# ---------------------------
-
-symlink_confirm_overwrite .vimrc
-
-do_if_confirmation "Do you want to set up vim Plugins?" '
-	mkdir -p "$HOME/.vim"
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	ln -s "$DOTFS/.vim/plugins.vim" "$HOME/.vim/plugins.vim"
-	cd "$CWD"
-'
-symlink_confirm_overwrite .ideavimrc
-
-
-do_if_confirmation "Do you want to Download Hack font?" 'curl "https://raw.githubusercontent.com/pyrho/hack-font-ligature-nerd-font/master/font/Hack%20Regular%20Nerd%20Font%20Complete%20Mono.ttf" Hack_Regular_Nerd_Font_Mono.ttf'
-
-do_if_confirmation "Do you want to install lazygit?" '
-  brew install lazygit
-	cd "$CWD"
-'
-symlink_confirm_overwrite Library/Application\ Support/lazygit/config.yml
